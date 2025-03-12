4d07567a94ce01f54d6c7f69d1f92cda8d1a12a1
antsthebul <anthony.allen.srt@gmail.com>
2025-03-12 14:28:52 -0400

    Add list and mapping recursion

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{read_dir, File},
-    io::{Read, Write}, path::Path,
+    io::{Read, Write}, path::Path, thread::panicking,
 };
 
 use regex::Regex;
@@ -34,54 +34,24 @@ mod tests{
         emitter.dump(doc).unwrap(); 
 
         out_str = out_str.strip_prefix("---\n").unwrap().to_string();
-        assert_eq!(out_str, yaml_as_str)
+        assert_eq!(out_str, yaml_as_str);
     }
     
-    #[test]
-    fn test_can_inject_yaml(){
-        let yaml_template = get_test_yaml_string("./src/test__inject_simple_template.yaml");
-        let child_template = get_test_yaml_string("./src/test__inject_simple__child_template.yaml");
-
-        let expected_template = get_test_yaml_string("./src/test__inject_simple_result.yaml");
-
-        let mut  docs =  Yaml::load_from_str(&yaml_template).unwrap();
-        let doc = &mut docs[0];
-
-        let mut ct_docs = Yaml::load_from_str(&child_template).unwrap();
-        let ct_doc = &mut ct_docs[0];
-        let ct_map = Yaml::Hash(ct_doc.as_hash().unwrap().clone());
-
-        for (_key, value) in doc.as_mut_hash().unwrap().iter_mut(){
-            if let Some(array) = value.as_mut_vec(){
-                array.push(ct_map.clone());
-            }
-        }
 
 
+    // #[test]
+    // fn test_yaml_is_injected(){
+    //     let yaml_template = get_test_yaml_string("./example/main.yaml");
+    //     let expected_yaml = get_test_yaml_string("./src/test__inject.yaml");
 
-        let mut out_str = String::new();
-        let mut emitter = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc).unwrap(); 
-        // let mut dest = File::create("test.yaml").unwrap();
-        // dest.write_all(&mut out_str.as_ref()).unwrap();
-        out_str = out_str.strip_prefix("---\n").unwrap().to_string();
-        assert_eq!(out_str, expected_template)
+    //     let docs =  Yaml::load_from_str(&yaml_template).unwrap();
+    //     let doc = &docs[0];
 
-    }
-
-    #[test]
-    fn test_yaml_is_injected(){
-        let yaml_template = get_test_yaml_string("./example/main.yaml");
-        let expected_yaml = get_test_yaml_string("./src/test__inject.yaml");
-
-        let docs =  Yaml::load_from_str(&yaml_template).unwrap();
-        let doc = &docs[0];
-
-        if (doc.is_hash()){
-            for (key, value) in doc.as_hash().into_iter().flat_map(|f|f){
-                println!("\ndata => {:?}", key);
-                if value.is_array(){
-                    for item in value.as_vec(){
+    //     if (doc.is_hash()){
+    //         for (key, value) in doc.as_hash().into_iter().flat_map(|f|f){
+    //             println!("\ndata => {:?}", key);
+    //             if value.is_array(){
+    //                 for item in value.as_vec(){
                         // if item.is_string() {
                         //     match func_regex.captures(item.as_str().unwrap()){
                         //         Some(v)=>{
@@ -102,82 +72,104 @@ mod tests{
                         // } else {
                         //     seq.push(item.clone());
                         // }
-                    }
-                }
-            }
-        }
-        assert_eq!(yaml_template, expected_yaml)
+        //             }
+        //         }
+        //     }
+        // }
+        // assert_eq!(yaml_template, expected_yaml)
+
+    // }
+
+    #[test]
+    fn test_build_section_list(){
+        let yaml_template = get_test_yaml_string("./src/test__inject_list_template.yaml");
+        let expected_output = get_test_yaml_string("./src/test__inject_list_result.yaml");
+        
+        let mut docs =  Yaml::load_from_str(&yaml_template).unwrap();
+        let doc = &mut docs[0];
+ 
+
+        build_body_v2(doc, None);
+        println!("omg {:?}", doc);
+        let mut out_str = String::new();
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap(); 
+
+        out_str = out_str.strip_prefix("---\n").unwrap().to_string();
+
+        
+        let mut des = File::create("lis_result.yaml").unwrap();
+        des.write_all(out_str.as_bytes()).unwrap();
+        assert_eq!(expected_output, out_str);
 
     }
 
     #[test]
-    fn test_build_section(){
-        let yaml_template = get_test_yaml_string("./example/main.yaml");
-        let docs =  Yaml::load_from_str(&yaml_template).unwrap();
-        let doc = &docs[0];
+    fn test_build_section_map(){
+        let yaml_template = get_test_yaml_string("./src/test__inject_map_template.yaml");
+        let expected_output = get_test_yaml_string("./src/test__inject_map_result.yaml");
+        
+        let mut docs =  Yaml::load_from_str(&yaml_template).unwrap();
+        let doc = &mut docs[0];
  
 
-        build_body_v2(doc.to_owned());
+        build_body_v2(doc, None);
+
+        let mut out_str = String::new();
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap(); 
+
+        out_str = out_str.strip_prefix("---\n").unwrap().to_string();
+
+        let mut des = File::create("mapper_result.yaml").unwrap();
+        des.write_all(out_str.as_bytes()).unwrap();
+
+        assert_eq!(expected_output, out_str);
+
     }
 }
 
-fn build_new_array(yaml:&Yaml, section_name:Option<&str>)->Vec<Yaml>{
-    let func_regex = Regex::new(r"(\w+)\((.+)\)").unwrap();
 
-    let mut new_arr = vec![];
-    for item in yaml.as_vec().unwrap(){
-        if item.is_string() {
-            match func_regex.captures(item.as_str().unwrap()){
-                Some(v)=>{
-                    let func = v.get(1).unwrap();
-                    let arg = v.get(2).unwrap().as_str();
-                    let mut args = arg.split(",");
+fn build_body_v2(yaml:&mut Yaml, current_section:Option<&str>){
 
-                    let lookups = match func.as_str(){
-                        "lookup"=>lookup(section_name, args.next().unwrap()),
-                        "lookup_file"=>vec![lookup_file(args.next().unwrap(), args.next().unwrap())],
-                        x=>panic!("{x} is not a function.")
-                    };
-                    new_arr.extend(lookups.into_iter());
-
-                },
-                None=>{new_arr.push(item.clone())}
-            }
-        } else {
-            new_arr.push(item.clone());
-        }
-    }
-    new_arr
-}
-
-fn build_body_v2(mut yaml:&Yaml, current_section:Option<&str>){
-    let func_regex = Regex::new(r"(\w+)\((.+)\)").unwrap();
- 
     if let Some(arr) = yaml.as_mut_vec(){
-        let mut ix = 0;
+        let mut total = 0;
+        let mut new_vec = vec![];
 
-        while ix < arr.len(){
+        for item in arr.into_iter(){
+            println!("Runing {:?}", item);
+            if let Some(item) = item.as_str(){
+                let (updated, data) = parse_function_for_section(current_section, item);
+                println!("we update date outer => {:?}", data);
+                if updated{
+                    new_vec.extend(data);
+                }else{
+                    new_vec.push(Yaml::String(item.to_owned()));
+
+                }
             
-            if let Some(item) = arr[ix].as_str(){
-                match func_regex.captures(item){
-                    Some(v)=>{
-                        let func = v.get(1).unwrap();
-                        let arg = v.get(2).unwrap().as_str();
-                        let mut args = arg.split(",");
-    
-                        let lookups = match func.as_str(){
-                            "lookup"=>lookup(current_section, args.next().unwrap()),
-                            "lookup_file"=>vec![lookup_file(args.next().unwrap(), args.next().unwrap())],
-                            x=>panic!("{x} is not a function.")
-                        };
-
-                        arr.splice(ix..=ix, lookups);
-
-                    },
-                    None=>()
-                };
+            }else{
+          
+                new_vec.push(item.clone());
             }
-            ix += 1;
+        }
+        *arr = new_vec;
+        println!("Im done {:?}", arr);
+    }
+    else if let Some(mapping) = yaml.as_mut_hash(){
+        for (_key, value) in mapping.iter_mut(){
+            let section_name = _key.as_str().unwrap();
+            if let Some(string_val) = value.as_str(){
+                let (updated, data) = parse_function_for_section(Some(section_name), string_val);
+                if updated && data.len() > 1{
+                    panic!("Use list syntax for add multiple object for section. '{}'", section_name)
+                }else if updated && data.len() > 0{
+                    *value = Yaml::Hash(data.iter().next().unwrap().as_hash().unwrap().to_owned());
+                    println!("Its updated {:?}", value);
+                }
+           }else if value.is_array(){
+            build_body_v2(value  , current_section);
+           }
         }
     }
     // if let Some(mapping) = yaml.as_mut_hash() {
@@ -232,10 +224,10 @@ fn main() {
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     // Serialize it to a YAML mapping.
-    let yamls = Yaml::load_from_str(&contents).unwrap();
-    let yaml = &yamls[0];
+    let mut yamls = Yaml::load_from_str(&contents).unwrap();
+    let yaml = &mut yamls[0];
 
-    result = build_section(yaml.clone(), result);
+    build_body_v2(yaml, None);
 
     let mut dest = File::create( args.output).unwrap();
     println!("Final => {:?}",&result);
@@ -244,9 +236,8 @@ fn main() {
 
 }
 
-fn yaml_from_file<P>(file:P) ->  Yaml
-    where P:AsRef<Path> {
-    let mut inner_file = File::open(file).unwrap();
+fn yaml_from_file(file:&str) ->  Yaml {
+    let mut inner_file = File::open(file).expect(&format!("check permissions for {file}"));
     let mut inner_contents = String::new();
     inner_file.read_to_string(&mut inner_contents).unwrap();
     Yaml::load_from_str(&inner_contents).unwrap()[0].clone()
@@ -269,7 +260,7 @@ fn lookup(section_name:Option<&str>, arg:&str)-> Vec<Yaml>{
     
     for dir in read_dir(loc).expect(&format!("check permissions/existence of {loc}")) {
         let dir = dir.unwrap();
-        let v = yaml_from_file(dir.path());
+        let v = yaml_from_file(dir.path().to_str().unwrap());
         // if v.is_sequence(){
         //     seq.extend(v.as_sequence().into_iter().flat_map(|f|f.to_owned()));
 
@@ -286,7 +277,7 @@ fn parse_function_for_section(section_name:Option<&str>,_str:&str)-> (bool,Vec<Y
     let func_regex = Regex::new(r"(\w+)\((.+)\)").unwrap();
     let mut updated = false;
     let mut seq =  vec![];
-    match func_regex.captures(_str){
+    match func_regex.captures(_str.trim()){
         Some(v)=>{
             updated = true;
             let func = v.get(1).unwrap();
@@ -295,7 +286,7 @@ fn parse_function_for_section(section_name:Option<&str>,_str:&str)-> (bool,Vec<Y
 
             let lookups = match func.as_str(){
                 "lookup"=>lookup(section_name, args.next().unwrap()),
-                "lookup_file"=>vec![lookup_file(args.next().unwrap(), args.next().unwrap())],
+                "lookup_file"=>lookup_file(args.next().unwrap(), args.next().unwrap()),
                 x=>panic!("{x} is not a function.")
             };
             seq.extend(lookups.into_iter());
@@ -303,15 +294,25 @@ fn parse_function_for_section(section_name:Option<&str>,_str:&str)-> (bool,Vec<Y
         },
         None=>{()}
     }
+    println!("kill {} {:?}", updated, seq);
     (updated, seq)
 }
 /// Used to lookup single object from file and return YAML
-fn lookup_file(dir:&str, file_name:&str) -> Yaml{
+fn lookup_file(mut dir:&str, mut file_name:&str) -> Vec<Yaml>{
     let mut result = String::new();
-    let complete_path = &format!("./{dir}/{file_name}");
+    println!("erm {} {}", dir, file_name);
+    dir = dir.trim().strip_prefix("'").unwrap().strip_suffix("'").unwrap();
+    file_name = file_name.trim().strip_prefix("'").unwrap().strip_suffix("'").unwrap();
+    let complete_path = &format!("{dir}/{file_name}");
+    println!("wow {}",complete_path);
     let path = Path::new(complete_path);
     // Just like template, we read in first
-    yaml_from_file(path)
+    let y = yaml_from_file(path.to_str().unwrap());
+    if !y.is_array(){
+        vec![y.to_owned()]
+    }else{
+        y.as_vec().unwrap().to_owned()
+    }
     // build out any necessary params
     // result = build_section(v, result);
 
@@ -319,44 +320,3 @@ fn lookup_file(dir:&str, file_name:&str) -> Yaml{
 
 
 
-fn build_section(yaml:Yaml, mut result:String,)-> String{
-    let func_regex = Regex::new(r"(\w+)\((.+)\)").unwrap();
-    // let mut emitter = YamlEmitter::new(&mut out_str);
-    for (key, section) in yaml.as_hash().iter().flat_map(|d| d.iter()) {
-        let section_name = key.as_str().unwrap();
-        println!("sec => {}", section_name);
-        result.push_str(&format! {"{section_name}:\n"});
-        if section.is_hash() {
-            result = build_section(section.clone(), result.clone());
-        } else if section.is_array() {
-            // Iterate over items
-            let mut seq = vec![];
-            for item in section.as_vec().iter().flat_map(|f| f.iter()) {
-                // if str then check if its a function
-                if item.is_string() {
-                    match func_regex.captures(item.as_str().unwrap()){
-                        Some(v)=>{
-                            let func = v.get(1).unwrap();
-                            let arg = v.get(2).unwrap().as_str();
-                            let mut args = arg.split(",");
-   
-                            let lookups = match func.as_str(){
-                                "lookup"=>lookup(Some(section_name), args.next().unwrap()),
-                                "lookup_file"=>vec![lookup_file(args.next().unwrap(), args.next().unwrap())],
-                                x=>panic!("{x} is not a function.")
-                            };
-                            seq.extend(lookups.into_iter());
-        
-                        },
-                        None=>{seq.push(item.clone())}
-                    }
-                } else {
-                    seq.push(item.clone());
-                }
-            }
-       
-        }
-    }
-    result
-
-}
